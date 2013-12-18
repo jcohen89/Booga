06dc74408e22638bd777756673a228a9c843c7ae
Vincent Laufer <vlaufer@uab.edu>
2013-12-18 12:48:39 -0600

    Create GSMTables.php

---
+++ GSMTables.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link type='text/css' rel='stylesheet' href='style.css'/>
+    <title>PHP!</title>
+  </head>
+  <body>
+  <?php
+  echo "Hello World"
+  ?>
+  </body>
+</html>
